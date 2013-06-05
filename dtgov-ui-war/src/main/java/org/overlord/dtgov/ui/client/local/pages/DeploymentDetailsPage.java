da3a46de37e041ff3803dd3687fc129bd92b7c73
Eric Wittmann <eric.wittmann@redhat.com>
2013-06-05 07:46:38 -0400

    First pass impl of the Deployment Lifecycle page.

---
+++ dtgov-ui-war/src/main/java/org/overlord/dtgov/ui/client/local/pages/DeploymentDetailsPage.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.overlord.dtgov.ui.client.local.pages;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.nav.client.local.PageState;
+import org.jboss.errai.ui.nav.client.local.TransitionAnchor;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.Templated;
+
+/**
+ * The Task Inbox page.
+ *
+ * @author eric.wittmann@redhat.com
+ */
+@Templated("/org/overlord/dtgov/ui/client/local/site/deploymentDetails.html#page")
+@Page(path="deploymentDetails")
+@Dependent
+public class DeploymentDetailsPage extends AbstractPage {
+
+    @PageState
+    private String uuid;
+
+    // Breadcrumbs
+    @Inject @DataField("back-to-dashboard")
+    TransitionAnchor<DashboardPage> backToDashboard;
+    @Inject @DataField("back-to-deployments")
+    TransitionAnchor<DeploymentsPage> backToDeployments;
+
+    /**
+     * Constructor.
+     */
+    public DeploymentDetailsPage() {
+    }
+
+}
--- dtgov-ui-war/src/main/java/org/overlord/dtgov/ui/client/local/services/DeploymentsRpcService.java
+++ dtgov-ui-war/src/main/java/org/overlord/dtgov/ui/client/local/services/DeploymentsRpcService.java
@@ -24,84 +24,67 @@ import org.jboss.errai.common.client.api.RemoteCallback;
 import org.overlord.dtgov.ui.client.local.services.rpc.DelegatingErrorCallback;
 import org.overlord.dtgov.ui.client.local.services.rpc.DelegatingRemoteCallback;
 import org.overlord.dtgov.ui.client.local.services.rpc.IRpcServiceInvocationHandler;
-import org.overlord.dtgov.ui.client.shared.beans.TaskActionEnum;
-import org.overlord.dtgov.ui.client.shared.beans.TaskBean;
-import org.overlord.dtgov.ui.client.shared.beans.TaskInboxFilterBean;
-import org.overlord.dtgov.ui.client.shared.beans.TaskInboxResultSetBean;
+import org.overlord.dtgov.ui.client.shared.beans.DeploymentBean;
+import org.overlord.dtgov.ui.client.shared.beans.DeploymentResultSetBean;
+import org.overlord.dtgov.ui.client.shared.beans.DeploymentsFilterBean;
 import org.overlord.dtgov.ui.client.shared.exceptions.DtgovUiException;
-import org.overlord.dtgov.ui.client.shared.services.ITaskInboxService;
+import org.overlord.dtgov.ui.client.shared.services.IDeploymentsService;
 
 /**
- * Client-side service for making RPC calls to the remote task inbox service.
+ * Client-side service for making RPC calls to the remote deployments service.
  *
  * @author eric.wittmann@redhat.com
  */
 @ApplicationScoped
-public class TaskInboxRpcService {
+public class DeploymentsRpcService {
 
     @Inject
-    private Caller<ITaskInboxService> remoteTaskInboxService;
+    private Caller<IDeploymentsService> remoteDeploymentsService;
 
     /**
      * Constructor.
      */
-    public TaskInboxRpcService() {
+    public DeploymentsRpcService() {
     }
 
     /**
-     * @see org.overlord.dtgov.ui.client.shared.services.ITaskInboxService#search(TaskInboxFilterBean, int)
+     * @see org.overlord.dtgov.ui.client.shared.services.IDeploymentsService#search(org.overlord.dtgov.ui.client.shared.beans.DeploymentsFilterBean, String, int)
      */
-    public void search(TaskInboxFilterBean filters, int page,
-            final IRpcServiceInvocationHandler<TaskInboxResultSetBean> handler) {
+    public void search(DeploymentsFilterBean filters, String searchText, int page,
+            final IRpcServiceInvocationHandler<DeploymentResultSetBean> handler) {
         // TODO only allow one search at a time.  If another search comes in before the previous one
         // finished, cancel the previous one.  In other words, only return the results of the *last*
         // search performed.
-        RemoteCallback<TaskInboxResultSetBean> successCallback = new DelegatingRemoteCallback<TaskInboxResultSetBean>(handler);
+        RemoteCallback<DeploymentResultSetBean> successCallback = new DelegatingRemoteCallback<DeploymentResultSetBean>(handler);
         ErrorCallback<?> errorCallback = new DelegatingErrorCallback(handler);
         try {
-            remoteTaskInboxService.call(successCallback, errorCallback).search(filters, page);
+            remoteDeploymentsService.call(successCallback, errorCallback).search(filters, searchText, page);
         } catch (DtgovUiException e) {
             errorCallback.error(null, e);
         }
     }
 
     /**
-     * @see org.overlord.dtgov.ui.client.shared.services.ITaskInboxService#get(String)
+     * @see org.overlord.dtgov.ui.client.shared.services.IDeploymentsService#get(String)
      */
-    public void get(String taskId, IRpcServiceInvocationHandler<TaskBean> handler) {
-        RemoteCallback<TaskBean> successCallback = new DelegatingRemoteCallback<TaskBean>(handler);
+    public void get(String uuid, IRpcServiceInvocationHandler<DeploymentBean> handler) {
+        RemoteCallback<DeploymentBean> successCallback = new DelegatingRemoteCallback<DeploymentBean>(handler);
         ErrorCallback<?> errorCallback = new DelegatingErrorCallback(handler);
         try {
-            remoteTaskInboxService.call(successCallback, errorCallback).get(taskId);
+            remoteDeploymentsService.call(successCallback, errorCallback).get(uuid);
         } catch (DtgovUiException e) {
             errorCallback.error(null, e);
         }
     }
 
     /**
-     * @see org.overlord.dtgov.ui.client.shared.services.ITaskInboxService#update(TaskBean)
+     * @see org.overlord.dtgov.ui.client.shared.services.IDeploymentsService#update(DeploymentBean)
      */
-    public void update(TaskBean task, IRpcServiceInvocationHandler<Void> handler) {
+    public void update(DeploymentBean deployment, IRpcServiceInvocationHandler<Void> handler) {
         RemoteCallback<Void> successCallback = new DelegatingRemoteCallback<Void>(handler);
         ErrorCallback<?> errorCallback = new DelegatingErrorCallback(handler);
         try {
-            remoteTaskInboxService.call(successCallback, errorCallback).update(task);
-        } catch (DtgovUiException e) {
-            errorCallback.error(null, e);
-        }
-    }
-
-    /**
-     * Executes the given action on the task.
-     * @param task
-     * @param action
-     * @param handler
-     */
-    public void executeAction(TaskBean task, TaskActionEnum action, IRpcServiceInvocationHandler<TaskBean> handler) {
-        RemoteCallback<TaskBean> successCallback = new DelegatingRemoteCallback<TaskBean>(handler);
-        ErrorCallback<?> errorCallback = new DelegatingErrorCallback(handler);
-        try {
-            remoteTaskInboxService.call(successCallback, errorCallback).executeAction(task, action);
+            remoteDeploymentsService.call(successCallback, errorCallback).update(deployment);
         } catch (DtgovUiException e) {
             errorCallback.error(null, e);
         }
