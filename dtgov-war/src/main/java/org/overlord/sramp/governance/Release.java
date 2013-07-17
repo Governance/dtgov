e063bb0bb24bb1646fa6738cb140308a1522df3a
Eric Wittmann <eric.wittmann@redhat.com>
2013-07-17 13:28:20 -0400

    Fixed a problem where the workflows jar version finder grabbed the wrong version when installed in EAP.

--- dtgov-war/src/main/java/org/overlord/sramp/governance/Release.java
+++ dtgov-war/src/main/java/org/overlord/sramp/governance/Release.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2001-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,13 +23,13 @@ import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 /**
- * 
+ *
  */
 public class Release {
 
-	private static final String JAR_NAME = "dt-gov-war";
+	private static final String JAR_NAME = "dtgov-task-api";
 	private static String governanceVersion = null;
-   
+
 	private Release () {
 	}
 
@@ -38,16 +38,16 @@ public class Release {
 			governanceVersion = getVersionFromManifest(JAR_NAME);
 		}
 		return governanceVersion;
-		
+
 	}
-	
+
 	public static String getVersionFromManifest(String jarName) {
 		Enumeration<URL> resEnum;
         try {
             resEnum = Thread.currentThread().getContextClassLoader().getResources(JarFile.MANIFEST_NAME);
             while (resEnum.hasMoreElements()) {
                 try {
-                    URL url = (URL) resEnum.nextElement();
+                    URL url = resEnum.nextElement();
                     if (url.toString().toLowerCase().contains(jarName)) {
                         InputStream is = url.openStream();
                         if (is != null) {
