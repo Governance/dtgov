1385eddf9e9fd4e6e81f26dd99d0a9ce5e0863e2
Eric Wittmann <eric.wittmann@redhat.com>
2013-08-01 15:23:18 -0400

    tweak to release script

--- release.sh
+++ release.sh
@@ -6,4 +6,4 @@ echo "##############################################"
 echo ""
 read -p "Release Version: " RELEASE_VERSION
 read -p "New Development Version: " DEV_VERSION
-mvn -e --batch-mode -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEV_VERSION clean release:prepare release:perform
+mvn -e --batch-mode -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEV_VERSION -DignoreSnapshots=true clean release:prepare release:perform
