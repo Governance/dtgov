bccdf46fb361d9d2c091737cc3bfdae3e69bcc1f
Kurt Stam <kstam@redhat.com>
2014-03-05 14:43:38 -0500

    https://issues.jboss.org/browse/DTGOV-107

---
+++ dtgov-installer/src/main/resources/updates/setenv.sh
@@ -0,0 +1,1 @@
+CATALINA_OPTS="-Xmx512M -XX:MaxPermSize=512m -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n -Dbtm.root=$CATALINA_HOME -Dbitronix.tm.configuration=$CATALINA_HOME/conf/btm-config.properties"
