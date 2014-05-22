98593bb136b7ee7113278c02c5d2ea5d1d1c0660
Eric Wittmann <eric.wittmann@gmail.com>
2014-05-22 07:23:33 -0400

    ensured tomcat is working

--- dtgov-installer/src/main/resources/updates/setenv.sh
+++ dtgov-installer/src/main/resources/updates/setenv.sh
@@ -1,1 +1,1 @@
-CATALINA_OPTS="-Xmx512M -XX:MaxPermSize=512m -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n -Dbtm.root=$CATALINA_HOME -Dbitronix.tm.configuration=$CATALINA_HOME/conf/btm-config.properties"
+CATALINA_OPTS="-Xmx512M -XX:MaxPermSize=512m -agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=n -Dbtm.root=$CATALINA_HOME -Dbitronix.tm.configuration=$CATALINA_HOME/conf/btm-config.properties"
