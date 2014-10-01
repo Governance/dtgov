2052172a8f8a777271e35267b389bd9a28c95968
dvirgiln <david.virgil.naranjo@gmail.com>
2014-10-01 16:16:24 +0200

    Including the workflow user information in the dtgov properties when it is deployed in fuse or fuse fabric.

--- dtgov-karaf-commands/src/main/java/org/overlord/dtgov/karaf/commands/ConfigureConstants.java
+++ dtgov-karaf-commands/src/main/java/org/overlord/dtgov/karaf/commands/ConfigureConstants.java
@@ -13,4 +13,18 @@ public interface ConfigureConstants {
 
     public static final String DTGOV_PROPERTIES_FILE_NAME = "dtgov.properties";
 
+    public static final String DTGOV_WORKFLOW_PASSWORD = "${dtgov.users.workflow.password}";
+    // FABRIC CONSTANTS
+
+    // Ui headers:
+
+    public static final String DTGOV_HEADER_HREF = "overlord.headerui.apps.dtgov.href";
+    public static final String DTGOV_HEADER_HREF_VALUE = "/dtgov-ui/";
+    public static final String DTGOV_HEADER_LABEL = "overlord.headerui.apps.dtgov.label";
+    public static final String DTGOV_HEADER_LABEL_VALUE = "Design Time";
+    public static final String DTGOV_HEADER_PRIMARY_BRAND = "overlord.headerui.apps.dtgov.primary-brand";
+    public static final String DTGOV_HEADER_PRIMARY_BRAND_VALUE = "JBoss Overlord";
+    public static final String DTGOV_HEADER_SECOND_BRAND = "overlord.headerui.apps.dtgov.secondary-brand";
+    public static final String DTGOV_HEADER_SECOND_BRAND_VALUE = "Governance";
+
 }
--- dtgov-karaf-commands/src/main/java/org/overlord/dtgov/karaf/commands/ConfigureFabricCommand.java
+++ dtgov-karaf-commands/src/main/java/org/overlord/dtgov/karaf/commands/ConfigureFabricCommand.java
@@ -15,6 +15,12 @@
  */
 package org.overlord.dtgov.karaf.commands;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.apache.felix.gogo.commands.Command;
 import org.overlord.commons.karaf.commands.configure.AbstractConfigureFabricCommand;
 
@@ -29,6 +35,16 @@ import org.overlord.commons.karaf.commands.configure.AbstractConfigureFabricComm
 public class ConfigureFabricCommand extends AbstractConfigureFabricCommand {
 
 
+    private static String DTGOV_PROFILE_PATH;
+
+    static {
+        if (File.separator.equals("/")) { //$NON-NLS-1$
+            DTGOV_PROFILE_PATH = "overlord/dtgov.profile"; //$NON-NLS-1$
+        } else {
+            DTGOV_PROFILE_PATH = "overlord\\dtgov.profile"; //$NON-NLS-1$
+        }
+    }
+
     /*
      * (non-Javadoc)
      *
@@ -37,12 +53,85 @@ public class ConfigureFabricCommand extends AbstractConfigureFabricCommand {
     @Override
     protected Object doExecute() throws Exception {
         super.doExecute();
-
-
+        addHeaderProperties();
+        configureDtgovProperties();
         return null;
     }
 
 
 
+    private void configureDtgovProperties() throws Exception {
+        InputStream is = this.getClass().getResourceAsStream("/" + ConfigureConstants.DTGOV_PROPERTIES_FILE_NAME);
+        Properties dtgovProps = new Properties();
+        dtgovProps.load(is);
+        for (Object key : dtgovProps.keySet()) {
+            String value = (String) dtgovProps.get(key);
+            if (value.contains(ConfigureConstants.DTGOV_WORKFLOW_PASSWORD)) {
+                dtgovProps.put(key, password);
+            }
+        }
+        File dtgovFile = new File(getDtgovPropertiesFilePath()); //$NON-NLS-1$
+
+        dtgovProps.store(new FileOutputStream(dtgovFile), "");
+    }
+
+    /**
+     * Adds the header properties.
+     *
+     * @throws Exception
+     *             the exception
+     */
+    private void addHeaderProperties() throws Exception {
+
+        String filePath = getOverlordPropertiesFilePath();
+
+        Properties props = new Properties();
+        InputStream is = null;
+        try {
+            is = new FileInputStream(new File(filePath));
+            props.load(is);
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(filePath);
+            props.setProperty(ConfigureConstants.DTGOV_HEADER_HREF, ConfigureConstants.DTGOV_HEADER_HREF_VALUE);
+            props.setProperty(ConfigureConstants.DTGOV_HEADER_LABEL, ConfigureConstants.DTGOV_HEADER_LABEL_VALUE);
+            props.setProperty(ConfigureConstants.DTGOV_HEADER_PRIMARY_BRAND, ConfigureConstants.DTGOV_HEADER_PRIMARY_BRAND_VALUE);
+            props.setProperty(ConfigureConstants.DTGOV_HEADER_SECOND_BRAND, ConfigureConstants.DTGOV_HEADER_SECOND_BRAND_VALUE);
+            props.store(out, null);
+
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    /**
+     * Gets the fabric sramp profile path.
+     *
+     * @return the fuse config path
+     */
+    public String getDtgovFabricProfilePath() {
+        StringBuilder fuse_config_path = new StringBuilder();
+        fuse_config_path.append(getFabricProfilesPath()).append(DTGOV_PROFILE_PATH).append(File.separator);
+        return fuse_config_path.toString();
+    }
+
+    /**
+     * Gets the sramp properties file path.
+     *
+     * @return the sramp properties file path
+     */
+    private String getDtgovPropertiesFilePath() {
+        StringBuilder fuse_config_path = new StringBuilder();
+        fuse_config_path.append(getDtgovFabricProfilePath()).append(ConfigureConstants.DTGOV_PROPERTIES_FILE_NAME);
+        return fuse_config_path.toString();
+    }
 
 }
